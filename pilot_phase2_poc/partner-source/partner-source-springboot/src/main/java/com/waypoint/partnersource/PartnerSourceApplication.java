3507dfd017e29fde76632e2c5318e9c7c5d30848
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2026-07-02 00:03:04 +0800

    Set up Spring Boot scaffold and CI workflow

---
+++ pilot_phase2_poc/partner-source/partner-source-springboot/src/main/java/com/waypoint/partnersource/PartnerSourceApplication.java
@@ -0,0 +1,13 @@
+package com.waypoint.partnersource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerSourceApplication.class, args);
+    }
+
+}
---
+++ pilot_phase2_poc/partner-source/partner-source-springboot/src/test/java/com/waypoint/partnersource/PartnerSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waypoint.partnersource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
